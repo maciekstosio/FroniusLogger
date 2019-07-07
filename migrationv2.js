28fbbcff36071ec3fa34bf465112aa64c9587cf7
Maciek Mać Mini <maciekstosio@icloud.com>
2019-07-07 17:37:36 +0200

    Add another migration

---
+++ migrationv2.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const config = require('./config');
+
+const read = filePath => {
+    try {
+        const data = fs.readFileSync(filePath, 'utf8')
+        return JSON.parse(data)
+    } catch(err) {
+        console.log(err)
+        return []
+    }
+};
+
+const write = (filePath, data) => {
+    fs.writeFile(filePath, JSON.stringify(data), function (err) {
+        if (err) throw err;
+        console.log('Saved data to file ' + filePath);
+      });
+};
+
+const log = async () => {
+    const readFile = read(config.logPath + process.argv[2] + '.log')
+    
+    const newData = {
+        total: 0,
+        logs: readFile.logs.map(item => ({
+            value: (item.Value || item.value) || 0,
+            timestamp: item.timestamp,
+        })),
+    }
+    
+    write(config.logPath + process.argv[2] + '.log', newData)
+}
+
+try {
+    log()
+} catch(err) {
+    console.log("Log fail", err)
+}
